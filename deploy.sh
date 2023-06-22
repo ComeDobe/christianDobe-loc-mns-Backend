8a682228698aa49af8655e46f55538bae5592b64
Christian DOBE <christiandobe@hotmail.fr>
2023-06-22 10:03:15 +0200

    mise en prod

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ bash mvnw package -P prod -e
 # Construire l'image Docker
 docker build --no-cache -t image-locmns .
 # Arreter le conteneur existant
-docker stop conteneur-spring-demo
+docker stop conteneur-locmns
 # Supprimer le conteneur existant
-docker rm -f conteneur-spring-demo
+docker rm -f conteneur-locmns
 # Lancer un nouveau conteneur
 docker run -d --net backend --ip 172.18.0.4 --name=conteneur-locmns -p 8080:8080 -v uploaded_files:/uploads image-locmns
