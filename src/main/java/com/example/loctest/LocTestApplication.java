de5ac3fb4ffac186b8f95ab610fa1a0adabe9c74
Christian DOBE <christiandobe@hotmail.fr>
2023-06-22 10:00:46 +0200

    mise en prod

--- src/main/java/com/example/loctest/LocTestApplication.java
+++ src/main/java/com/example/loctest/LocTestApplication.java
@@ -2,6 +2,8 @@ package com.example.loctest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,10 +11,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 
-public class LocTestApplication {
+public class LocTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LocTestApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LocTestApplication.class);
+    }
 }
