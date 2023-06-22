0797aff7a9b1a9d07d39c6ee8924d4beaa419854
Christian DOBE <christiandobe@hotmail.fr>
2023-06-22 14:57:50 +0200

    mise en prod

--- src/main/java/com/example/loctest/security/MonUserDetailsService.java
+++ src/main/java/com/example/loctest/security/MonUserDetailsService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-//import org.springframework.security.core.userdetails.User;
 import java.util.Optional;
 
 @Service
