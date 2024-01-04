53929eaeb31b59b3e099b1320c0d3bd7818ab5d3
Christian DOBE <117773557+ComeDobe@users.noreply.github.com>
2024-01-04 14:54:34 +0100

    une mise à jour de l'application properties

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-##!/bin/bash
-## Mettre à jour le code source
-#git pull
-## Construire le projet avec Maven
-#bash mvnw package -P prod -e
-## Construire l'image Docker
-#docker build --no-cache -t image-locmns .
-## Arreter le conteneur existant
-#docker stop conteneur-locmns
-## Supprimer le conteneur existant
-#docker rm -f conteneur-locmns
-## Lancer un nouveau conteneur
+#!/bin/bash
+# Mettre à jour le code source
+git pull
+# Construire le projet avec Maven
+bash mvnw package -P prod -e
+# Construire l'image Docker
+docker build --no-cache -t image-backendlocmns .
+# Arreter le conteneur existant
+docker stop conteneur-backendlocmns
+# Supprimer le conteneur existant
+docker rm -f conteneur-backendlocmns
+# Lancer un nouveau conteneur
 #docker run -d --net backend --ip 172.18.0.4 --name=conteneur-locmns -p 8080:8080 -v uploaded_files:/uploads image-locmns
